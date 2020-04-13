39baece5be39f4aca39bf73f400925725f959cef
Nikita Kalinnikov <niki9888985@mail.ru>
2020-04-14 01:23:54 +0300

    Автоматический рассчет размера ортографической камеры

--- Assets/Scripts/CameraCentering.cs
+++ Assets/Scripts/CameraCentering.cs
@@ -5,10 +5,30 @@
 /// </summary>
 public class CameraCentering : MonoBehaviour
 {
+	[SerializeField]
+	float deltaPositionYForGUI = 0f;
+
+	float width;
+	float height;
+
     void Start()
 	{
-		float newPosX = (float)TileMap.mapWidht / 2 - 0.5f;
-		float newPosY = (float)TileMap.mapHeight / 2 - 0.5f;
+		width = TileMap.mapWidht;
+		height = TileMap.mapHeight;
+
+		setCenteredPosition();
+		setOrthograficSize();
+	}
+
+	void setCenteredPosition()
+	{
+		float newPosX = width / 2 - 0.5f;
+		float newPosY = height / 2 - 0.5f + deltaPositionYForGUI;
 		transform.position = new Vector3(newPosX, newPosY, -1);
+	}
+
+	void setOrthograficSize()
+	{
+		Camera.main.orthographicSize = width >= height ? width / 2 : height / 2;
 	}
 }
